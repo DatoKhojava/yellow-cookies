beae961976daac147b029e5eb9faa8cfea00e612
Dato Khojava <datokhojava@Datos-MacBook-Air.local>
2024-09-26 21:47:55 +0400

    added second task

--- app.js
+++ app.js
@@ -22,3 +22,37 @@ Array.prototype.myFilter = myFilter;
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evens = numbers.myFilter((num) => num % 2 === 0);
 console.log(evens); // Output: [2, 4]
+
+// -------------------------------------------------------------------
+
+const JSConfirm = window.confirm;
+const JSAlert = window.alert;
+const JSPrompt = window.prompt;
+
+window.alert = function (message) {
+  return JSConfirm(message);
+};
+
+window.prompt = function (message, defaultValue) {
+  JSAlert(message);
+  return defaultValue;
+};
+
+window.confirm = function (message) {
+  return JSPrompt(message, "");
+};
+
+const result = window.alert("Are you fan of Lord of the Rings?");
+console.log("Alert returned:", result);
+
+const userInput = window.prompt("What race do you represent?", "Human");
+console.log("User input:", userInput);
+
+const userResponse = window.confirm("Speak friend and enter:");
+if (userResponse == null || userResponse == "") {
+  console.log("Speak!!!!");
+} else if (userResponse === "Mellon") {
+  console.log("The door is open");
+} else {
+  console.log("You are not welcomed in Moria");
+}
