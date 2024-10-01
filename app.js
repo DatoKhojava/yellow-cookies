ddde7b83e77dcab42bc39394181e6f101f129acc
Dato Khojava <datokhojava@Datos-MacBook-Air.local>
2024-10-01 16:47:23 +0400

    third task

--- app.js
+++ app.js
@@ -1,58 +1,35 @@
-function myFilter(callback, obj) {
-  // check on error
-  if (typeof callback !== "function") {
-    throw new TypeError(`${callback} isnt a function! plase pass a function`);
+const myIterration = { from: 1, to: 10 };
+
+function iteration(obj) {
+  let from = obj.from;
+  let to = obj.to;
+
+  if (to < from) {
+    throw new Error("TO must be '>' then FROM");
   }
 
-  const res = [];
+  if (typeof from !== "number") {
+    throw new Error("invalid starting point");
+  }
 
-  for (let i = 0; i < this.length; i++) {
-    if (i in this) {
-      if (callback.call(obj, this[i], i, this)) {
-        res.push(this[i]);
-      }
+  if (typeof to !== "number") {
+    throw new Error("invalid ending point");
+  }
+
+  if(from === to) {
+    throw new Error("there is no range between FROM and TO")
+  }
+
+  //in case if starting point is 0 i decided to start with 0 (even it isnt natural number)
+  if (from === 0) {
+    for (let i = 0; i < to; i++) {
+      console.log(i);
+    }
+  } else {
+    for (let i = 0; i < to; i++) {
+      console.log(i + 1);
     }
   }
-
-  return res;
 }
 
-Array.prototype.myFilter = myFilter;
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evens = numbers.myFilter((num) => num % 2 === 0);
-console.log(evens); // Output: [2, 4]
-
-// -------------------------------------------------------------------
-
-const JSConfirm = window.confirm;
-const JSAlert = window.alert;
-const JSPrompt = window.prompt;
-
-window.alert = function (message) {
-  return JSConfirm(message);
-};
-
-window.prompt = function (message, defaultValue) {
-  JSAlert(message);
-  return defaultValue;
-};
-
-window.confirm = function (message) {
-  return JSPrompt(message, "");
-};
-
-const result = window.alert("Are you fan of Lord of the Rings?");
-console.log("Alert returned:", result);
-
-const userInput = window.prompt("What race do you represent?", "Human");
-console.log("User input:", userInput);
-
-const userResponse = window.confirm("Speak friend and enter:");
-if (userResponse == null || userResponse == "") {
-  console.log("Speak!!!!");
-} else if (userResponse === "Mellon") {
-  console.log("The door is open");
-} else {
-  console.log("You are not welcomed in Moria");
-}
+iteration(myIterration);
