71ad17a6e65b4ff7d91ea25f111c52c347711781
Dato Khojava <datokhojava@Datos-MacBook-Air.local>
2024-10-17 22:53:35 +0400

    seventh task

--- app.js
+++ app.js
@@ -1,57 +1,19 @@
-class DataHandler {
-  #URL = `https://jsonplaceholder.typicode.com`;
+const button = document.getElementById("randomButton");
 
-  constructor() {
-    this.posts = [];
-  }
-
-  async fetchPosts() {
-    try {
-      const response = await fetch(`${this.#URL}/posts`);
-
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      data.map((post) => this.posts.push(post));
-
-      return Promise.resolve(this.posts);
-    } catch (error) {
-      return Promise.reject("Something went wrong ", error);
-    }
-  }
-
-  listPosts() {
-    if (this.posts.length > 0) {
-      return this.posts.sort(function (a, b) {
-        return a.title.localeCompare(b.title);
-      });
-    }
-
-    return "There is no recors in Posts arrya, its seems you deleted it, try to fetch one more time!";
-  }
-
-  getPost(id) {
-    if (this.posts.length > 0) {
-      return this.posts.find((post) => post.id === id);
-    }
-
-    return "There is no recors in Posts arrya, its seems you deleted it, try to fetch one more time!";
-  }
-
-  clearPosts() {
-    return (this.posts = []);
-  }
+function randomPosition() {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+  button.style.position = "absolute";
+  button.style.left = `${x}px`;
+  button.style.top = `${y}px`;
 }
 
-(async () => {
-  const data = new DataHandler();
+button.addEventListener("mouseenter", () => {
+  if (Math.random() < 0.5) {
+    randomPosition();
+  }
+});
 
-  await data.fetchPosts();
-  console.log(data.listPosts());
-  console.log(data.getPost(30));
-  data.clearPosts();
-  console.log(data.listPosts());
-})();
+button.addEventListener("click", () => {
+  randomPosition();
+});
